5f5e7b58bfea4d495d159916ed77265d771b2779
Djordje Ljubinkovic <djordje.ljubinkovic@lotusflare.com>
2020-09-22 14:18:38 +0200

    Creates all necessary views.

---
+++ MovieRecommender/Cells/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCell.swift
+//  MovieRecommender
+//
+//  Created by Djordje Ljubinkovic on 9/22/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+}
